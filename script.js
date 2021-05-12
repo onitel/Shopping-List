7c64e0cb72a19ee5e0dc6d3eeb011c72c0faba4a
Catalin Babalau <b.catalin.777@gmail.com>
2021-05-12 19:09:03 +0300

    changes

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function createListElement() {
 	input.value = "";
 }
 
-// aceasta functie de mai jos e special facuta ca sa stearga de pe lista cu ajutorul lui buton ceea ce adug eu nou
 function removeParent(evt){
 	evt.target.parentNode.remove();
 }
@@ -43,42 +42,22 @@ function addListAfterKeypress(event) {
 }
 
 function addLineIfClicked(e) {
-  // e.target is our targetted element.
-                // try doing console.log(e.target.nodeName), it will result LI
 				if(e.target && e.target.nodeName == "LI") {
-// 					console.log(e.target.id + " was clicked");
 					e.target.classList.toggle("done");
 				}
 }
 
-// { for (var i = 0; i < removeButtons.length; i++) {
-// 	btndelete[i].addEventListener('click', function(e) {
-// 	//   e.currentTarget.parentNode.remove();
-// 	//   this.closest('.single').remove() // in modern browsers in complex dom structure
-// 	//   this.parentNode.remove(); //this refers to the current target element 
-// 	  e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-// 	}, false);
-//   }
-// }
-
-// aceasta functie cat si cele de mai sus sunt pentru a sterge produsele deja listate pe pagina
 	Array.from(removeButtons).forEach((removeButton) => {
 	removeButton.addEventListener('click', () => {
 	  removeButton.parentNode.remove();
 	});
   });
 
-// asta e o funcite care sterge totul din lista  
 function deleteList(){
 	parent.innerHTML = "";
 	}
 
-// aici mai jos am mai multe functii care daca apas klic sau dau enter, si mai sunt si alte event reference
-// nu doar "click" sau "keypress" ap activeaza functia de alaturi cum ar fi addListAfterClik sau addListAfterKeypress
 button.addEventListener("click", addListAfterClick);
-
 input.addEventListener("keypress", addListAfterKeypress);
-
 toDoList.addEventListener("click", addLineIfClicked);
-
 deleteAll.addEventListener("click", deleteList);
